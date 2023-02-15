b483d8692f0ae1712ba0bd49173a4dbbb44c2d1f
Noela-Murugi <murugin254@gmail.com>
2023-02-15 08:52:08 +0300

    update home.tsx file

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,3 +4,33 @@ import styled from "styled-components";
 import { selectDarts } from "../features/Dart/dartSlice";
 import Section from "./Section";
 
+interface HomeInterface {
+   homeRef: React.RefObject<HTMLDivElement>;
+}
+function Home(props: HomeInterface) {
+   const { homeRef } = props;
+   const darts = useSelector(selectDarts);
+
+   return (
+      <Container ref={homeRef}>
+         {darts.map((dart) => (
+            <Section
+               homeRef={homeRef}
+               key={dart.id}
+               id={dart.id}
+               title={dart.title}
+               backgroundImg={dart.backgroundImg}
+            />
+         ))}
+      </Container>
+   );
+}
+
+export default Home;
+
+const Container = styled.div`
+   overflow-y: auto;
+   text-align: center;
+   height: 100vh;
+   scroll-snap-type: y mandatory;
+`;
