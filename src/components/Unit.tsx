6bffa48e5c9c0e7b4a4ad27208bfcce995489f67
Noela-Murugi <murugin254@gmail.com>
2023-02-15 09:08:06 +0300

    add styling

--- src/components/Unit.tsx
+++ src/components/Unit.tsx
@@ -66,3 +66,60 @@ function Section(props: SectionInterface) {
 
 export default Section;
 
+const Wrap = styled.div`
+   scroll-snap-align: start;
+   height: 100vh;
+   background-position: center;
+   background-size: cover;
+   background-repeat: no-repeat;
+   background-image: ${(props: { bgImage: string }) =>
+      `url("images/${props.bgImage}")`};
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   align-items: center;
+`;
+
+const ItemSet = styled.div`
+   padding-top: 15vh;
+   test-align: center;
+`;
+
+const ButtonGroup = styled.div`
+   display: flex;
+   margin-bottom: 30px;
+   @media (max-width: 768px) {
+      flex-direction: column;
+   }
+`;
+
+const LeftButton = styled.div`
+   background-color: rgba(23, 26, 32, 0.8);
+   height: 40px;
+   width: 256px;
+   color: white;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   border-radius: 100px;
+   opacity: 0.85;
+   text-transform: uppercase;
+   font-size: 12px;
+   cursor: pointer;
+   margin: 8px;
+`;
+const Buttons = styled.div``;
+
+const RightButton = styled(LeftButton)`
+   background: white;
+   opacity: 0.65;
+   color: black;
+`;
+
+const DownArrow = styled.img`
+   margin-top: 20px;
+   height: 40px;
+   animation: animateDown infinite 1.5s;
+   overflow-x: hidden;
+`;
+
